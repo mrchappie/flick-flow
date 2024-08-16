8793ccf29c3a1a41403bc0ff42ded8551e9dec1b
Alexandru BOSCU <boscu20@gmail.com>
2024-08-16 17:56:51 +0300

    implement lazy loading per layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import {
   AdminRoutes,
   DefaultRoutes,
   UserProfileRoutes,
 } from './routes/RoutesContext';
 import { Route, Routes } from 'react-router-dom';
-import DefaultLayout from 'pages/layouts/defaultLayout';
-import AdminLayout from 'pages/admin/adminLayout';
-import UserProfileLayout from 'pages/layouts/userProfileLayout';
 import { Flip, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRunner from 'utils/AppRunner';
 import { ModalProvider } from 'utils/modals/ModalContext';
 import ModalManager from 'utils/modals/ModalManager';
+
+const DefaultLayout = lazy(() => import('pages/layouts/defaultLayout'));
+const AdminLayout = lazy(() => import('pages/admin/adminLayout'));
+const UserProfileLayout = lazy(() => import('pages/layouts/userProfileLayout'));
 
 function App() {
   return (
